b54c5c48aa22784b1c2604c7d19c58fd15a980fa
kenteg <kenteg91@gmail.com>
2016-11-12 23:36:10 +0600

    declared methods

--- src/com/luxoft/birdsstore/Store.java
+++ src/com/luxoft/birdsstore/Store.java
@@ -1,8 +1,14 @@
 package com.luxoft.birdsstore;
 
+import com.luxoft.birdsstore.model.Goods;
+
+import java.util.Set;
+
 /**
  * @author Khrishpens Viktor
  *         created Ноябрь 12 2016
  */
 public interface Store {
+    void addItem(Goods item);
+    Set<Goods> getItems();
 }
