1a6562db17b116ee6f1f09e1b6ae277aa25d4d4c
kenteg <kenteg91@gmail.com>
2016-11-12 19:19:56 +0600

    create Singleton

--- src/com/luxoft/birdsstore/BirdsStore.java
+++ src/com/luxoft/birdsstore/BirdsStore.java
@@ -9,7 +9,19 @@ import java.util.Set;
 
 public class BirdsStore {
     String name;
-Set<Goods> items = new HashSet<Goods>();
+    Set<Goods> items = new HashSet<Goods>();
+    private static BirdsStore instance;
+
+    private BirdsStore(){
+
+    }
+
+    public static BirdsStore getInstance(){
+        if (instance==null){
+            instance = new BirdsStore();
+        }
+        return instance;
+    }
     public static void main(String[] args) {
 	// write your code here
     }
