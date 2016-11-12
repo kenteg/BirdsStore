7a7e4e6f68f4d4350cca1ee613d461d2f0718680
kenteg <kenteg91@gmail.com>
2016-11-12 18:44:58 +0600

    moved

--- src/com/birdsstore/BirdsStore.java
+++
@@ -1,8 +0,0 @@
-package com.birdsstore;
-
-public class BirdsStore {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/com/luxoft/birdsstore/BirdsStore.java
@@ -0,0 +1,16 @@
+package com.luxoft.birdsstore;
+
+import com.luxoft.birdsstore.model.Bird;
+import com.luxoft.birdsstore.model.Goods;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BirdsStore {
+    String name;
+Set<Goods> items = new HashSet<Goods>();
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
