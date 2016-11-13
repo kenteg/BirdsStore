7d1579f75369b6bb680e3925d2945c902224fac6
kenteg <kenteg91@gmail.com>
2016-11-13 23:17:32 +0600

    created

---
+++ src/com/luxoft/birdsstore/exceptions/NotEnoughGoodsException.java
@@ -0,0 +1,12 @@
+package com.luxoft.birdsstore.exceptions;
+
+/**
+ * @author Khrishpens Viktor
+ *         created Ноябрь 13 2016
+ */
+public class NotEnoughGoodsException extends Exception {
+
+    public NotEnoughGoodsException(String message) {
+        super(message);
+    }
+}
