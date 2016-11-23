209211c74e471b9b0d82cb389b4124f2af913cea
kenteg <kenteg91@gmail.com>
2016-11-23 23:27:10 +0600

    created gui classes

---
+++ src/com/luxoft/gui/StoreForm.java
@@ -0,0 +1,11 @@
+package com.luxoft.gui;
+
+import javax.swing.*;
+
+/**
+ * @author Khrishpens Viktor
+ *         created Ноябрь 23 2016
+ */
+public class StoreForm {
+    private JTable table1;
+}
