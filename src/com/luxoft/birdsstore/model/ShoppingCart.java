84e83e7c44e624a8f8b9a395c62962c72116cd17
VKhrishpens <kenteg91@gmail.com>
2016-11-25 15:17:58 +0600

    added shopping cart

---
+++ src/com/luxoft/birdsstore/model/ShoppingCart.java
@@ -0,0 +1,11 @@
+package com.luxoft.birdsstore.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by VKhrishpens on 11/25/2016.
+ */
+public class ShoppingCart {
+    List<Goods> goods = new ArrayList<>();
+}
