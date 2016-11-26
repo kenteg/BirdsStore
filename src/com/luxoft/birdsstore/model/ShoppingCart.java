9315d8f4fc943581f0d1816a1c6b1d495a7bfc87
kenteg <kenteg91@gmail.com>
2016-11-26 19:26:36 +0600

    created and implemented ShoppingCart

--- src/com/luxoft/birdsstore/model/ShoppingCart.java
+++ src/com/luxoft/birdsstore/model/ShoppingCart.java
@@ -1,5 +1,7 @@
 package com.luxoft.birdsstore.model;
 
+import com.luxoft.birdsstore.exceptions.NotEnoughGoodsException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,4 +10,29 @@ import java.util.List;
  */
 public class ShoppingCart {
     List<Goods> goods = new ArrayList<>();
+
+    public void add(Goods good){
+        goods.add(good);
+    }
+
+    public void remove(Goods good){
+        goods.remove(good);
+    }
+
+    public void pay(){
+        try {
+            for(Goods item:goods) item.sell(1);
+        }
+        catch (NotEnoughGoodsException e){
+            e.printStackTrace();
+        }
+            
+        }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        for(Goods item:goods) sb.append(item+"\n");
+        return sb.toString();
+    }
 }
