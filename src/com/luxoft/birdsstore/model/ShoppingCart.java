0963fa73f2138837b7e2eee935e66e62fc6ab3f3
kenteg <kenteg91@gmail.com>
2016-11-28 00:10:50 +0600

    add copy constructor

--- src/com/luxoft/birdsstore/model/ShoppingCart.java
+++ src/com/luxoft/birdsstore/model/ShoppingCart.java
@@ -2,7 +2,9 @@ package com.luxoft.birdsstore.model;
 
 import com.luxoft.birdsstore.exceptions.NotEnoughGoodsException;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
 /**
@@ -11,7 +13,29 @@ import java.util.List;
 public class ShoppingCart {
     List<Goods> goods = new ArrayList<>();
 
+    public ShoppingCart() {
+        this.totalPrice = new Money(BigDecimal.ZERO, Currency.getInstance("USD"));
+    }
+
+    public ShoppingCart(ShoppingCart copy){
+        this.goods = new ArrayList<>(copy.getItems());
+        this.totalPrice = copy.getTotal();
+    }
+
+    public String getTotalPrice() {
+        return totalPrice.toString();
+    }
+
+    public Money getTotal() {
+        return totalPrice;
+    }
+
+    private Money totalPrice;
+
     public void add(Goods good){
+        BigDecimal temp= totalPrice.getAmount();
+        temp= good.getPrice().getAmount().add(temp);
+        totalPrice = Money.dollars(temp.toString());
         goods.add(good);
     }
 
