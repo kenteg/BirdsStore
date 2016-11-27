ede4d2fe9435b29cb20aac1cdaffdde8154288a1
kenteg <kenteg91@gmail.com>
2016-11-28 00:11:18 +0600

    orders added

--- src/com/luxoft/birdsstore/BirdsStore.java
+++ src/com/luxoft/birdsstore/BirdsStore.java
@@ -4,6 +4,7 @@ import com.luxoft.birdsstore.exceptions.NotEnoughGoodsException;
 import com.luxoft.birdsstore.model.Bird;
 import com.luxoft.birdsstore.model.Goods;
 import com.luxoft.birdsstore.model.Money;
+import com.luxoft.birdsstore.model.Order;
 import com.luxoft.gui.StoreForm;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,6 +20,13 @@ public class BirdsStore implements Store {
     String name;
 
     Set<Goods> items = new HashSet<Goods>();
+
+    @Override
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    List<Order> orders = new ArrayList<>();
     private static BirdsStore instance;
 
     private BirdsStore() {
@@ -43,6 +51,10 @@ public class BirdsStore implements Store {
 
     public void addItem(Goods item) {
         items.add(item);
+    }
+
+    public void addOrder(Order order){
+        orders.add(order);
     }
 
     public List<Goods> getItems() {
--- src/com/luxoft/birdsstore/Store.java
+++ src/com/luxoft/birdsstore/Store.java
@@ -1,6 +1,7 @@
 package com.luxoft.birdsstore;
 
 import com.luxoft.birdsstore.model.Goods;
+import com.luxoft.birdsstore.model.Order;
 
 import java.util.List;
 import java.util.Set;
@@ -11,6 +12,8 @@ import java.util.Set;
  */
 public interface Store {
     void addItem(Goods item);
+    void addOrder(Order order);
+    List<Order> getOrders();
     List<Goods> getItems();
     void sell(Goods item,int amount);
 }
